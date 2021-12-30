f3ab742d1566965a46fa1cc44c21dd3dd32f3a7e
sasender <sasender123@gmail.com>
2021-12-30 16:36:54 +0530

    new line ading

--- testscript.sh
+++ testscript.sh
@@ -3,7 +3,7 @@ echo "this script is from github"
 echo "this is practise code"
 echo "jhon is one of my riend"
 echo "navarasa to nine"
-NAME=$sasenderreddy
+NAME="sasenderreddy"
 echo "$NAME"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
