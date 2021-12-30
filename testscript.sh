4cfceb1449ebcbe5eeacd6d3ccdf7a36f6c86241
sasender <sasender123@gmail.com>
2021-12-30 14:41:16 +0530

    new line ading

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,7 @@
 echo "this script is from github"
 echo "this is practise code"
 echo "jhon is one of my riend"
+echo "navarasa"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "completed"
